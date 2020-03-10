50f99ed96c5d89d0abcaef473e5a4970f578505e
billy <billy.ml.tsai@mail.foxconn.com>
2020-03-10 14:44:39 +0800

    add disconnet retry function

--- observer.py
+++ observer.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import http.client
 
 from urllib import request,error
 from urllib.request import urlopen
@@ -41,20 +42,24 @@ if __name__ == "__main__":
     def downWeb(url):
         headers = {'User-Agent': 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
-        try:
-            req = request.Request(url, headers=headers)
-            html_data = urlopen(url).read().decode()
-        except error.HTTPError as e:
-            print("get url Fail")
-            print(e.reason, e.code, e.headers)
-            return
-        except error.URLError as e:
-            print(e.reason)
-            return
-        else:
-            # print('Request Successfully')
-            soup = BeautifulSoup(html_data, 'html.parser')
-            return soup.find_all('tr', {"class": "custDataGridRow", "bgcolor": "White"})
+        while 1:
+            try:
+                req = request.Request(url, headers=headers)
+                html_data = urlopen(url).read().decode()
+            except error.HTTPError as e:
+                print("get url Fail")
+                print(e.reason, e.code, e.headers)
+                return
+            except error.URLError as e:
+                print(e.reason)
+                return
+            except  http.client.HTTPException as e:
+                print("HTTPException retry!!")
+                time.sleep(1)
+            else:
+                # print('Request Successfully')
+                soup = BeautifulSoup(html_data, 'html.parser')
+                return soup.find_all('tr', {"class": "custDataGridRow", "bgcolor": "White"})
 
     def combineMsg(name,qtime,price,updown):
         msg = """\n%s
